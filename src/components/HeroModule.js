5da61e88285a0badccb32abdd697b1459280bccc
rafa <rafaelsistem87@gmail.com>
2021-02-24 16:00:15 -0700

    Refactoring6

--- src/components/HeroModule.js
+++ src/components/HeroModule.js
@@ -1,30 +1,5 @@
 import CardModule from './CardModule'
-
-import styled from 'styled-components'
-
-const ImageContainer = styled.div`
-  display:block;
-  position: relative;
-  width: 90%;
-  margin: 0 0;
-  box-sizing: border-box;
-  overflow: hidden;
-  background: Darkred;
-  margin: 1% 0;
-  width: 100%;
-  text-align: left;
-  padding: 1% 2%;
-  float: left;
-`
-
-const HeroContainer = styled.div`
-    width: 100%;
-    margin: 1% 0;
-    box-sizing: border-box;
-    display: flex;
-    flex-direction: column;
-`
-
+import {HeroContainer,ImageContainer} from './styles/styledHeroModule'
 const HeroModule = () => {
       return (
 
---
+++ src/components/styles/styledHeroModule.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+
+export const ImageContainer = styled.div`
+  display:block;
+  position: relative;
+  width: 90%;
+  margin: 0 0;
+  box-sizing: border-box;
+  overflow: hidden;
+  background: Darkred;
+  margin: 1% 0;
+  width: 100%;
+  text-align: left;
+  padding: 1% 2%;
+  float: left;
+`
+
+export const HeroContainer = styled.div`
+    width: 100%;
+    margin: 1% 0;
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+`
+
