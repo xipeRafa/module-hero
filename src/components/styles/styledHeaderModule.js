82741f8d70556a3cc94723954480822ca910624d
rafa <rafaelsistem87@gmail.com>
2021-02-24 15:55:47 -0700

    Refactoring4

---
+++ src/components/styles/styledHeaderModule.js
@@ -0,0 +1,27 @@
+import styled from "styled-components"
+
+export const HeaderOne = styled.h1`
+   margin-bottom: 0;
+`
+export const HeaderSix = styled.h6`
+   margin: 0;
+   margin-bottom: -10px;
+`
+export const MaxWidthContainer = styled.div`
+   max-width: 90%;
+   float: left;
+   margin: 1% 0;
+   width: 100%;
+   text-align: left;
+   padding: 1% 2%;
+   float: left;
+`
+
+export const HeaderContainer = styled.div`
+   width: 100%;
+   margin: 1% 0;
+   box-sizing: border-box;
+   display: flex;
+   flex-direction: column;
+`
+
