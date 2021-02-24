46e03732ff093caa8518c7652efffbaec40507c5
rafa <rafaelsistem87@gmail.com>
2021-02-24 16:07:38 -0700

    Refactoring8

--- src/components/TextModule.js
+++ src/components/TextModule.js
@@ -1,28 +1,4 @@
-import styled from "styled-components";
-
-const TextBannerP = styled.p`     
-   font-size: 14px;
-   margin-top: 0;
-   margin-bottom: 1.2%;
-`
-
-const MaxWidthContainer = styled.div`
-   max-width: 90%;
-   float: left;
-   margin: 1% 0;
-   width: 100%;
-   text-align: left;
-   padding: 1% 2%;
-   float: left;
-`
-
-const TextContainer = styled.div`
-   width: 100%;
-   margin: 1% 0;
-   box-sizing: border-box;
-   display: flex;
-   flex-direction: column;
-`
+import {TextContainer,MaxWidthContainer,TextBannerP} from "./styles/styledTextModule"
 
 const TextModule = () => {
       return (
---
+++ src/components/styles/styledTextModule.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const TextBannerP = styled.p`     
+   font-size: 14px;
+   margin-top: 0;
+   margin-bottom: 1.2%;
+`
+
+export const MaxWidthContainer = styled.div`
+   max-width: 90%;
+   float: left;
+   margin: 1% 0;
+   width: 100%;
+   text-align: left;
+   padding: 1% 2%;
+   float: left;
+`
+
+export const TextContainer = styled.div`
+   width: 100%;
+   margin: 1% 0;
+   box-sizing: border-box;
+   display: flex;
+   flex-direction: column;
+`
+
